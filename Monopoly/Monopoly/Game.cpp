97ab5a2ff8decfc5d881de804584b39e64b1d517
HiOrange428 <B10715060@mail.ntust.edu.tw>
2019-06-02 23:44:21 +0800

    [新增]路障、move與路障的互動

--- Monopoly/Monopoly/Game.cpp
+++ Monopoly/Monopoly/Game.cpp
@@ -9,9 +9,21 @@ Game::Game(string fileName = "basemap") {
 	this->mapFileName = fileName;
 	this->gameLife = true;
 	this->whosTurn = 0;
+	this->allPlayers = 0;
+	this->mapName = "";
+	this->remainTurn = 0;
 
 	// loading Map from file
 	this->loadMap();
+}
+
+Game::~Game() {
+	this->allPlayers = 0;
+	this->mapName = "";
+	this->mapFileName = "";
+	this->gameLife = false;
+	this->whosTurn = 0;
+	this->remainTurn = 0;
 }
 
 void Game::start() {
@@ -29,9 +41,6 @@ void Game::start() {
 		if (sites[i].owner != -1)
 			Display::printOwnEstate(sites[i], players);
 	}
-	//this->openOptions();
-	//system("pause");
-	//Display::rollDiceAnimate(2);
 }
 
 bool Game::isGameAlive() {
@@ -55,7 +64,37 @@ void Game::process() {
 		//Display::setConsoleCursorCoordinate(210, 40);
 		//cout << "０";
 
-		// 全畫面更新
+		/// 確認玩家數量&剩餘回合
+		playerNum = 0;
+		for (int i = 0; i < players.size(); i++) {
+			if (players[i].money >= 0) {
+				playerNum++;
+			}
+		}
+		/// 勝負判定開始
+		if (playerNum == 1 || this->remainTurn <= 0) {
+			Display::setColor(7, 0);
+			system("cls");
+			system("mode con cols=50 lines=10");
+			Player winner(0, 0, 0, 0, 0, 0);
+			int max = 0;
+			for (int i = 0; i < players.size(); i++) {
+				if (players[i].money > max) {
+					winner = players[i];
+					max = players[i].money;
+				}
+			}
+			cout << "Player" << winner.playerID << " is winner.\n";
+			this->gameLife = false;
+			system("pause");
+			return;
+		}
+		/// 勝負判定結束
+
+		/// 全畫面更新
+		Display::setColor();
+		system("cls");
+		Display::printBoard();
 		Display::printRightSpace(allPlayers);
 		Display::printCurrentPlayer(whosTurn + 1);
 		Display::printRound(remainTurn);
@@ -79,27 +118,36 @@ void Game::process() {
 				//請輸入欲擲出點數
 			}
 
+			/// 等待輸入開始
+			Display::setConsoleCursorCoordinate(144, 26);
 			cout << "請按任意鍵骰動骰子．．．";
 			while (commandPress = _getch()) {
 				if (commandPress == KEYBOARD_ESCAPE) {
-					this->openOptions();
+					int temp = this->openOptions();
+					if (temp == 2) {
+						return;
+					}
+					Display::setConsoleCursorCoordinate(144, 26);
+					cout << "請按任意鍵骰動骰子．．．";
 				}
 				else {
 					break;
 				}
 			}
+			/// 等待輸入結束
 
 			int point = playerIter->RollADice();
-			// 顯示骰子動畫
-			Display::rollDiceAnimate(point);
-			Display::clearPlayLog();
+			Display::rollDiceAnimate(point);									// 顯示骰子動畫
+			Display::clearPlayLog();											// 清除PlayLog
 			cout << "現在骰到了 " << point;
 			Display::setConsoleCursorCoordinate(144, 27);
 			cout << "玩家 " << playerIter->playerID + 1 << "從【" << sites[playerIter->location].name << "】";
 			Display::setConsoleCursorCoordinate(144, 28);
 			cout << "移動到";
 
+
 			playerIter->Move(point, sites);					// 移動玩家
+
 			int currentLocation = playerIter->location;
 
 			Display::setConsoleCursorCoordinate(150, 28);
@@ -114,34 +162,65 @@ void Game::process() {
 			case ESTATE:
 				if (sites[currentLocation].owner == -1) {//無主地
 					if (playerIter->money >= sites[currentLocation].firstPrice) {//若達成買地產條件，詢問是否置產(玩家現金足夠)
+
 						Display::setConsoleCursorCoordinate(144, 30);
 						cout << "【" << sites[currentLocation].name << "】目前沒有地主，想成為它（她）的主人嗎?(y/n)：";
-						char temp;
+						string temp;
 						cin >> temp;
-						if (temp == 'y' || temp == 'Y') {
+						if (temp == "y" || temp == "Y") {
 							playerIter->ProchaseAnEstate(currentLocation, sites);
-							Display::setConsoleCursorCoordinate(144, 31);
-							cout << "你成為了【" << sites[currentLocation].name << "】的master，恭喜你";
+
 							for (int i = 0; i < 28; i++) {
 								if (sites[i].owner != -1)
 									Display::printOwnEstate(sites[i], players);
 							}
+
+							Display::setConsoleCursorCoordinate(144, 31);
+							cout << "你成為了【" << sites[currentLocation].name << "】的Master，恭喜你";
 						}
 						else {
 							Display::setConsoleCursorCoordinate(144, 31);
 							cout << "你拒絕購買【" << sites[currentLocation].name << "】";
 						}
+
 					}
 				}
 				else {//有主地
 					//若達成地產升級條件，詢問是否升級(自己地)
 					if (sites[currentLocation].owner == playerIter->playerID && sites[currentLocation].estateLevel < 3) {
-						//如果回答yes(尚未實作)
-						playerIter->UpgradeAnEstate(currentLocation, sites);
+						
+						Display::setConsoleCursorCoordinate(144, 30);
+						cout << "你是【" << sites[currentLocation].name << "】的Master，想讓它（她）升級嗎?(y/n)：";
+						string temp;
+						cin >> temp;
+						if (temp == "y" || temp == "Y") {
+							playerIter->UpgradeAnEstate(currentLocation, sites);
+
+							for (int i = 0; i < 28; i++) {
+								if (sites[i].owner != -1)
+									Display::printOwnEstate(sites[i], players);
+							}
+
+							Display::setConsoleCursorCoordinate(144, 31);
+							cout << "恭喜你讓【" << sites[currentLocation].name << "】更加臣服於你";
+						}
+						else {
+							Display::setConsoleCursorCoordinate(144, 31);
+							cout << "你拒絕讓【" << sites[currentLocation].name << "】升級";
+						}
+						
 					}
 					//若踩到其他玩家之地產，進行付費(他人地)
 					else if (sites[currentLocation].owner != playerIter->playerID) {
+
+						Display::setConsoleCursorCoordinate(144, 30);
+						cout << "【" << sites[currentLocation].name << "】是玩家 "<< sites[currentLocation].owner+1 <<"的土(老)地(婆)，必須繳交過路費";
+						Display::setConsoleCursorCoordinate(144, 31);
+						cout << "你的金額從＄" << setw(10) << playerIter->money << "→＄";
 						playerIter->PayForTheToll(currentLocation, sites, players);
+						Display::setConsoleCursorCoordinate(170, 31);
+						cout << setw(10) << playerIter->money;
+
 					}
 				}
 				break;
@@ -153,83 +232,60 @@ void Game::process() {
 			default:
 				break;
 			}
-
-
-
 		}
 		else {//不可移動
 			playerIter->cannotMove--;
 		}
 		// 玩家行為結束
 
+		/// 等待輸入開始
 		Display::setConsoleCursorCoordinate(144, 40);
 		cout << "請按任意鍵繼續．．．";
 		while (commandPress = _getch()) {
 			if (commandPress == KEYBOARD_ESCAPE) {
-				this->openOptions();
+				int temp = this->openOptions();
+				if (temp == 2) {
+					return;
+				}
+				Display::setConsoleCursorCoordinate(144, 40);
+				cout << "請按任意鍵繼續．．．";
 			}
 			else {
 				break;
 			}
 		}
+		/// 等待輸入結束
 
-		//playerIter->money -= 100000;
+		// 破產測試
+		if (playerIter->playerID==0&&playerIter->money>0)
+			playerIter->money -= 100000;
 
-		// 破產判定開始
-		vector<Player> fakePlayers;
-		for (auto i = players.begin(); i != players.end(); i++) {
-			if (i->money >= 0) {
-				fakePlayers.push_back(*i);
+		/// 破產判定開始
+		for (int i = 0; i < players.size(); i++) {
+			if (players[i].money < 0) {
+				// 刪除地點的擁有者
+				for (int j = 0; j < players[i].estate.size(); j++) {
+					sites[players[i].estate[j].location].estateLevel = 0;
+					sites[players[i].estate[j].location].owner = -1;
+				}
+				// 刪除破產者的擁有地
+				players[i].estate.erase(players[i].estate.begin(), players[i].estate.end());
 			}
-			else {
+		}
+		/// 破產判定結束
 
+		/// 尋找下個玩家開始
+		do {
+			this->whosTurn++;
+			if (this->whosTurn % this->players.size() == 0) {
+				this->remainTurn--;
+				this->whosTurn %= this->players.size();
 			}
-		}
-		players = fakePlayers;
-		playerNum = players.size();
-		if (playerNum == 0) {
-			this->gameLife = false;
-			return;
-		}
-		// 破產判定結束
-
-		// 勝利判定開始
-		playerNum = players.size();
-		if (playerNum == 1 || this->remainTurn == 1) {
-			system("cls");
-			auto i = players.begin();
-			for (; i != players.end(); i++)
-				if (i->money >= 0)
-					break;
-			cout << "Player" << i->playerID << " is winner.\n";
-			this->gameLife = false;
-			system("pause");
-			return;
-		}
-		// 勝利判定結束
-
-		// 尋找下個玩家
-		this->whosTurn++;
-		if (this->whosTurn % this->players.size() == 0) {
-			this->remainTurn--;
-			this->whosTurn %= this->players.size();
-		}
-		playerIter = players.begin() + this->whosTurn;
-		
-
-
+			playerIter = players.begin() + this->whosTurn;
+		} while (playerIter->money < 0);
+		/// 尋找下個玩家結束
 	}
-	/*
-	while (command = _getch()) {
-		if (command == KEYBOARD_ESCAPE) {
-			this->openOptions();
-			return;
-		}
-		else {
-			return;
-		}
-	}
-	*/
+
 }
 
 void Game::loadMap() {
@@ -307,7 +363,7 @@ void Game::loadMap() {
 		}
 
 
-		// 角色排序開始
+		/// 角色排序開始
 		for (int i = 0; i < players.size() - 1; i++) {
 			for (int j = 0; j < players.size() - 1; j++) {
 				if (players[j].playerID > players[j + 1].playerID) {
@@ -317,14 +373,145 @@ void Game::loadMap() {
 				}
 			}
 		}
-		// 角色排序結束
+		/// 角色排序結束
 
 	}
 	inputFile.close();
 }
 
-void Game::openOptions() {
+int Game::openOptions() {
 	Display::showGameOptions();
-	//this->gameLife = false;
-	return;
+	Display::setConsoleCursorCoordinate(14, 32);
+	Display::setColor(150);
+	cout << "★";
+	int commandPress = 0, y = 32;
+	while (commandPress = _getch()) {
+		if (commandPress == KEYBOARD_DOWN) {
+			Display::setConsoleCursorCoordinate(14, y);
+			Display::setColor(240);
+			cout << "　";
+			y += 4;
+		}
+		else if (commandPress == KEYBOARD_UP) {
+			Display::setConsoleCursorCoordinate(14, y);
+			Display::setColor(240);
+			cout << "　";
+			y -= 4;
+		}
+		else if (commandPress == KEYBOARD_ENTER) {
+			if (y == 32) {
+				/// 繼續遊戲
+				Display::setColor();
+				system("cls");
+				Display::printBoard();
+				Display::printRightSpace(allPlayers);
+				Display::printCurrentPlayer(whosTurn + 1);
+				Display::printRound(remainTurn);
+				Display::printEstate(sites);
+				Display::printPlayersStatus(players);
+				Display::printPlayerStep(players);
+				for (int i = 0; i < 28; i++) {
+					if (sites[i].owner != -1)
+						Display::printOwnEstate(sites[i], players);
+				}
+				return 0;
+			}
+			else if (y == 36) {
+				/// 儲存遊戲
+				// 清除畫面
+				Display::setColor(7, 0);
+				system("cls");
+				system("mode con cols=50 lines=10");
+				Display::setConsoleCursorCoordinate(0, 0);
+				cout << "請輸入欲儲存檔名(輸入exit返回)：\n";
+				string saveFileName = "";
+				cin >> saveFileName;
+
+				if (saveFileName != "exit") {
+					fstream output("Map/" + saveFileName + ".txt",ios::out);
+					if (output.is_open()) {
+						int playerNum = 0;
+						for (int i = 0; i < players.size(); i++) {
+							if (players[i].money >= 0) {
+								playerNum++;
+							}
+						}
+						output << saveFileName << " " << this->remainTurn << " " << playerNum << endl;
+
+						for (int i = 0; i < sites.size(); i++) {
+							output << setw(2) << setfill('0') << i << " " << sites[i].name << " " << sites[i].type;
+							if (sites[i].type == 1) {
+								output << " " << sites[i].firstPrice
+									<< " " << sites[i].initialToll
+									<< " " << sites[i].firstStageToll
+									<< " " << sites[i].secondStageToll
+									<< " " << sites[i].thirdStageToll;
+							}
+							output << endl;
+						}
+						output << "playerstate " << this->whosTurn << endl;
+						for (int i = 0; i < this->players.size(); i++) {
+							output << this->players[i].playerID
+								<< " " << setw(2) << setfill('0') << this->players[i].location;
+							output << " " << this->players[i].money;
+							for (int j = 0; j < this->players[i].estate.size(); j++) {
+								output << " " << setw(2) << setfill('0') << this->players[i].estate[j].location;
+								output << " " << this->players[i].estate[j].estateLevel;
+							}
+							output << endl;
+						}
+
+						output.close();
+					}
+				}
+				
+				Display::setColor();
+				system("cls");
+				system("mode con cols=220 lines=50");
+				Display::printBoard();
+				Display::printRightSpace(allPlayers);
+				Display::printCurrentPlayer(whosTurn + 1);
+				Display::printRound(remainTurn);
+				Display::printEstate(sites);
+				Display::printPlayersStatus(players);
+				Display::printPlayerStep(players);
+				for (int i = 0; i < 28; i++) {
+					if (sites[i].owner != -1)
+						Display::printOwnEstate(sites[i], players);
+				}
+
+				return 1;
+			}
+			else if (y==40) {
+				/// 結束遊戲
+				this->gameLife = false;
+				Display::setColor(7, 0);
+				return 2;
+			}
+		}
+		else if (commandPress == KEYBOARD_ESCAPE) {
+			Display::setColor();
+			system("cls");
+			Display::printBoard();
+			Display::printRightSpace(allPlayers);
+			Display::printCurrentPlayer(whosTurn + 1);
+			Display::printRound(remainTurn);
+			Display::printEstate(sites);
+			Display::printPlayersStatus(players);
+			Display::printPlayerStep(players);
+			for (int i = 0; i < 28; i++) {
+				if (sites[i].owner != -1)
+					Display::printOwnEstate(sites[i], players);
+			}
+			return 0;
+		}
+		y = (y < 32) ? 40 : y;
+		y = (y > 40) ? 32 : y;
+		Display::setConsoleCursorCoordinate(14, y);
+		Display::setConsoleCursorCoordinate(14, y);
+		Display::setColor(150);
+		cout << "★";
+
+	}
+	return 0;
 }
